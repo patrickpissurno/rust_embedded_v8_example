189d1140ada9214fec8f18575864134f4945f9b1
Patrick Pissurno <contato@patrickpissurno.com.br>
2020-09-28 17:19:42 -0300

    Some refactoring

---
+++ src/js.rs
@@ -0,0 +1,57 @@
+use rusty_v8 as v8;
+
+pub mod log;
+pub mod require;
+
+pub fn run_script(scope: &mut v8::ContextScope<v8::HandleScope>, script: &str) -> String {
+    let code = v8::String::new(scope, script).unwrap();
+    //println!("javascript code: {}", code.to_rust_string_lossy(scope));
+
+    let script = v8::Script::compile(scope, code, None).unwrap();
+    let result = script
+        .run(scope)
+        .expect("Uncaught exception while executing the script");
+    let result = result.to_string(scope).unwrap();
+
+    //println!("result: {}\n", result.to_rust_string_lossy(scope));
+    result.to_rust_string_lossy(scope)
+}
+
+#[macro_export]
+macro_rules! run_script_to_object {
+    ($scope:expr, $script:expr) => {{
+        let script = format!("JSON.stringify({})", $script);
+        let res = run_script($scope, &script);
+        serde_json::from_str(&res).expect("Failed to deserialize JSON")
+    }};
+}
+
+pub fn init(){
+    let platform = v8::new_default_platform().unwrap();
+    v8::V8::initialize_platform(platform);
+    v8::V8::initialize();
+}
+
+pub fn set_globals(context: &v8::Local<v8::Context>, scope: &mut v8::ContextScope<v8::HandleScope>) {
+    let global = context.global(scope);
+
+    macro_rules! set_func {
+        ($name:expr, $callback:expr) => {{
+            let fn_template = v8::FunctionTemplate::new(scope, $callback);
+            let func = fn_template
+                .get_function(scope)
+                .expect("Unable to create function");
+            let key = v8::String::new(scope, $name).unwrap();
+            global.set(scope, key.into(), func.into());
+        }};
+    }
+
+    set_func!("log", log::log);
+    set_func!("require", require::require);
+
+    set_func!("get42", |scope: &mut v8::HandleScope,
+                        _args: v8::FunctionCallbackArguments,
+                        mut rv: v8::ReturnValue| {
+        rv.set(v8::Integer::new(scope, 42).into());
+    });
+}
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,14 @@
-#[macro_use]
+//#[macro_use]
 extern crate conrod;
 extern crate find_folder;
 use conrod::backend::glium::glium::{self, Surface};
 use conrod::{widget, Colorable, Positionable, Widget};
 use rusty_v8 as v8;
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize};
 use std::collections::HashMap;
 
-mod core;
-
-macro_rules! run_script_to_object {
-    ($scope:expr, $script:expr) => {{
-        let script = format!("JSON.stringify({})", $script);
-        let res = run_script($scope, &script);
-        serde_json::from_str(&res).expect("Failed to deserialize JSON")
-    }};
-}
+mod js;
+use js::{ run_script };
 
 #[derive(Deserialize)]
 struct Text {
@@ -24,9 +17,7 @@ struct Text {
 }
 
 fn main() {
-    let platform = v8::new_default_platform().unwrap();
-    v8::V8::initialize_platform(platform);
-    v8::V8::initialize();
+    js::init();
 
     let isolate = &mut v8::Isolate::new(Default::default());
 
@@ -34,7 +25,7 @@ fn main() {
     let context = v8::Context::new(scope);
     let scope = &mut v8::ContextScope::new(scope, context);
 
-    set_globals(&context, scope);
+    js::set_globals(&context, scope);
 
     const WIDTH: u32 = 400;
     const HEIGHT: u32 = 200;
@@ -63,6 +54,8 @@ fn main() {
 
     run_script(scope, "let screen1 = require('./js/screen1')");
 
+    // executes the setup function of the current screen in order to generate
+    // the ids that conrod requires
     {
         let ids_as_string: Vec<String> = run_script_to_object!(scope, "screen1.setup()");
         for key in ids_as_string {
@@ -75,6 +68,7 @@ fn main() {
         {
             let ui = &mut ui.set_widgets();
 
+            // executes the draw function of the current screen
             let txts: Vec<Text> = run_script_to_object!(scope, "screen1.draw()");
 
             for txt in txts {
@@ -117,42 +111,4 @@ fn main() {
             }
         }
     }
-}
-
-fn set_globals(context: &v8::Local<v8::Context>, scope: &mut v8::ContextScope<v8::HandleScope>) {
-    let global = context.global(scope);
-
-    macro_rules! set_func {
-        ($name:expr, $callback:expr) => {{
-            let fn_template = v8::FunctionTemplate::new(scope, $callback);
-            let func = fn_template
-                .get_function(scope)
-                .expect("Unable to create function");
-            let key = v8::String::new(scope, $name).unwrap();
-            global.set(scope, key.into(), func.into());
-        }};
-    }
-
-    set_func!("log", core::log::log);
-    set_func!("require", core::require::require);
-
-    set_func!("get42", |scope: &mut v8::HandleScope,
-                        _args: v8::FunctionCallbackArguments,
-                        mut rv: v8::ReturnValue| {
-        rv.set(v8::Integer::new(scope, 42).into());
-    });
-}
-
-fn run_script(scope: &mut v8::ContextScope<v8::HandleScope>, script: &str) -> String {
-    let code = v8::String::new(scope, script).unwrap();
-    //println!("javascript code: {}", code.to_rust_string_lossy(scope));
-
-    let script = v8::Script::compile(scope, code, None).unwrap();
-    let result = script
-        .run(scope)
-        .expect("Uncaught exception while executing the script");
-    let result = result.to_string(scope).unwrap();
-
-    //println!("result: {}\n", result.to_rust_string_lossy(scope));
-    result.to_rust_string_lossy(scope)
 }
