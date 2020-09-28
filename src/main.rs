00ddcfb2a1d331a58277b4ccb5f3e033408d0dc0
Patrick Pissurno <contato@patrickpissurno.com.br>
2020-09-28 16:39:02 -0300

    Reformatted the code

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-#[macro_use] extern crate conrod;
+#[macro_use]
+extern crate conrod;
 extern crate find_folder;
 use conrod::backend::glium::glium::{self, Surface};
-use conrod::{widget, Positionable, Colorable, Widget};
+use conrod::{widget, Colorable, Positionable, Widget};
 use rusty_v8 as v8;
-use std::collections::HashMap;
 use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 
 mod core;
 
@@ -12,7 +13,6 @@ macro_rules! run_script_to_object {
     ($scope:expr, $script:expr) => {{
         let script = format!("JSON.stringify({})", $script);
         let res = run_script($scope, &script);
-
         serde_json::from_str(&res).expect("Failed to deserialize JSON")
     }};
 }
@@ -41,11 +41,11 @@ fn main() {
 
     let mut events_loop = glium::glutin::EventsLoop::new();
     let window = glium::glutin::WindowBuilder::new()
-                    .with_title("V8 + Conrod")
-                    .with_dimensions(WIDTH, HEIGHT);
+        .with_title("V8 + Conrod")
+        .with_dimensions(WIDTH, HEIGHT);
     let context = glium::glutin::ContextBuilder::new()
-                    .with_vsync(true)
-                    .with_multisampling(4);
+        .with_vsync(true)
+        .with_multisampling(4);
     let display = glium::Display::new(window, context, &events_loop).unwrap();
     let mut ui = conrod::UiBuilder::new([WIDTH as f64, HEIGHT as f64]).build();
 
@@ -77,7 +77,7 @@ fn main() {
 
             let txts: Vec<Text> = run_script_to_object!(scope, "screen1.draw()");
 
-            for txt in txts{
+            for txt in txts {
                 let id = ids.get(&txt.id).unwrap();
 
                 widget::Text::new(&txt.text)
@@ -87,7 +87,6 @@ fn main() {
                     .set(*id, ui);
             }
         }
-            
         // Render the `Ui` and then display it on the screen.
         if let Some(primitives) = ui.draw_if_changed() {
             renderer.fill(&display, primitives, &image_map);
@@ -100,15 +99,16 @@ fn main() {
         let mut events = Vec::new();
         events_loop.poll_events(|event| events.push(event));
 
-        for event in events{
+        for event in events {
             match event {
-                glium::glutin::Event::WindowEvent { event, ..} => match event {
-                    glium::glutin::WindowEvent::Closed |
-                    glium::glutin::WindowEvent::KeyboardInput {
-                        input: glium::glutin::KeyboardInput {
-                            virtual_keycode: Some(glium::glutin::VirtualKeyCode::Escape),
-                            ..
-                        },
+                glium::glutin::Event::WindowEvent { event, .. } => match event {
+                    glium::glutin::WindowEvent::Closed
+                    | glium::glutin::WindowEvent::KeyboardInput {
+                        input:
+                            glium::glutin::KeyboardInput {
+                                virtual_keycode: Some(glium::glutin::VirtualKeyCode::Escape),
+                                ..
+                            },
                         ..
                     } => break 'main,
                     _ => (),
@@ -119,17 +119,15 @@ fn main() {
     }
 }
 
-fn set_globals(context: &v8::Local<v8::Context>, scope: &mut v8::ContextScope<v8::HandleScope>){
+fn set_globals(context: &v8::Local<v8::Context>, scope: &mut v8::ContextScope<v8::HandleScope>) {
     let global = context.global(scope);
 
     macro_rules! set_func {
         ($name:expr, $callback:expr) => {{
             let fn_template = v8::FunctionTemplate::new(scope, $callback);
-        
             let func = fn_template
                 .get_function(scope)
                 .expect("Unable to create function");
-    
             let key = v8::String::new(scope, $name).unwrap();
             global.set(scope, key.into(), func.into());
         }};
@@ -138,7 +136,9 @@ fn set_globals(context: &v8::Local<v8::Context>, scope: &mut v8::ContextScope<v8
     set_func!("log", core::log::log);
     set_func!("require", core::require::require);
 
-    set_func!("get42", |scope: &mut v8::HandleScope, _args: v8::FunctionCallbackArguments, mut rv: v8::ReturnValue|{
+    set_func!("get42", |scope: &mut v8::HandleScope,
+                        _args: v8::FunctionCallbackArguments,
+                        mut rv: v8::ReturnValue| {
         rv.set(v8::Integer::new(scope, 42).into());
     });
 }
@@ -148,7 +148,9 @@ fn run_script(scope: &mut v8::ContextScope<v8::HandleScope>, script: &str) -> St
     //println!("javascript code: {}", code.to_rust_string_lossy(scope));
 
     let script = v8::Script::compile(scope, code, None).unwrap();
-    let result = script.run(scope).expect("Uncaught exception while executing the script");
+    let result = script
+        .run(scope)
+        .expect("Uncaught exception while executing the script");
     let result = result.to_string(scope).unwrap();
 
     //println!("result: {}\n", result.to_rust_string_lossy(scope));
